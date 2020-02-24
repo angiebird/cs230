01cf02139a19c52cc692ccd8209d23a4bba47ac4
Ubuntu <ubuntu@ip-172-31-33-254.us-west-2.compute.internal>
2020-02-24 03:35:57 +0000

    Add run_hrnet.sh

---
+++ HRNet-Semantic-Segmentation/run_hrnet.sh
@@ -0,0 +1,1 @@
+python tools/test.py --cfg experiments/cityscapes/test_seg_hrnet_w48.yaml TEST.MODEL_FILE data/pretrained_models/hrnet_w48_cityscapes_cls19_1024x2048_trainset.pth TEST.SCALE_LIST 1.0,
