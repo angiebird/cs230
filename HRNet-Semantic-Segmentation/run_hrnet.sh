3e1b0d0d98fd7d3e3a39117ff1372654ead892d1
Angie Chiang <angieyayabird@gmail.com>
2020-03-12 20:55:44 +0000

    Add bdd100k_resize.yaml

--- HRNet-Semantic-Segmentation/run_hrnet.sh
+++ HRNet-Semantic-Segmentation/run_hrnet.sh
@@ -1,1 +1,1 @@
-python tools/test.py --cfg experiments/cityscapes/bdd100k.yaml TEST.MODEL_FILE data/pretrained_models/hrnet_w48_cityscapes_cls19_1024x2048_trainset.pth TEST.SCALE_LIST 1.0,
+python tools/test.py --cfg experiments/bdd100k/bdd100k.yaml TEST.MODEL_FILE data/pretrained_models/hrnet_w48_cityscapes_cls19_1024x2048_trainset.pth TEST.SCALE_LIST 1.0,
